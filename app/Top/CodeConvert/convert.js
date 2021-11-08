1ccc70f8b48118e6946e014ef3d34be437274b99
Miao Zhuang <1060950782@163.com>
2021-11-08 16:06:59 +0800

    add

--- app/Top/CodeConvert/convert.js
+++ app/Top/CodeConvert/convert.js
@@ -1,16 +1,38 @@
-function object(obj){
-    let each = ``;
-    for(let key in obj){
-        if(obj[key]){
-            each+= `
-                ${key}:${obj[key]}
-            }`;
-        }
-    }
-    let str = `{
+import { useState, useEffect, useRef } from "react";
+import { Form, Select, Dropdown } from "antd";
 
-    }`
-}
-export {
+const FreeForm = Form.create()((props) => {
+    const { form } = props;
+    const { getFieldDecorator } = form;
+    // 表单属性
+    const formProps = {};
+    return (
+        <div className="free-form">
+            <Form {...formProps}>
+                <div
+                    className="form-item-container"
+                    style={{
+                        display: "flex",
+                        flexDirection: "undefined",
+                    }}
+                >
+                    <Form.Item
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 8 }}
+                        label={"测试"}
+                        rules={[{ required: true }]}
+                        wrapperCol={{ span: 8 }}
+                        labelCol={{ span: 8 }}
+                    >
+                        {getFieldDecorator("undefined", {
+                            rules: [{ required: true }],
+                        })(<Select placeholder={"请选择"} />)}
+                    </Form.Item>
 
-}
+                    <Select placeholder={"请选择"} />
+                </div>
+            </Form>
+        </div>
+    );
+});
+export default FreeForm;
